d506ec96e0fa87945f9757202e6ba6ce122adcc8
Vitor Maciel <vitormf@yahoo.com>
2015-04-08 20:10:18 -0300

    Alterando autor da classe

--- Samples/Inlocomedia-Ads-Sample/Dialogs/UBENotificationAdAlertView.h
+++ Samples/Inlocomedia-Ads-Sample/Dialogs/UBENotificationAdAlertView.h
@@ -2,7 +2,7 @@
 //  UBENotificationAdAlertView.h
 //  InLocoMedia-Ads-Sample-IOS
 //
-//  Created by Vitor Maciel on 4/8/15.
+//  Created by Ubee on 4/8/15.
 //  Copyright (c) 2015 Ubee. All rights reserved.
 //
 
